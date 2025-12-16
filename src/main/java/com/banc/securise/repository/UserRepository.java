c8ced29ccbe918d6cfb836a9f458b50242e2f7a6
Azzedine-zemmari <azzedinezemmari@gmail.com>
2025-12-16 19:21:32 +0100

    cree userRepository avec customize method findByEmail

---
+++ src/main/java/com/banc/securise/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.banc.securise.repository;
+
+import com.banc.securise.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<Integer, User> {
+    Optional<User> findByEmail(String email);
+}
