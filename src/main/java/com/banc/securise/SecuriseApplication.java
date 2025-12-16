7d9f8b99795a458649d25d5389e75349db2cdcff
Azzedine-zemmari <azzedinezemmari@gmail.com>
2025-12-16 10:56:05 +0100

    start

---
+++ src/main/java/com/banc/securise/SecuriseApplication.java
@@ -0,0 +1,13 @@
+package com.banc.securise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuriseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuriseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/banc/securise/SecuriseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banc.securise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuriseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
