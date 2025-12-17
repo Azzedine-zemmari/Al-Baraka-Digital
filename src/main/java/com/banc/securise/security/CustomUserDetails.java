ae28a503683d2281e7d7ecc02f0edb2c25c3290b
Azzedine-zemmari <azzedinezemmari@gmail.com>
2025-12-17 11:09:37 +0100

    Create the custom user details

---
+++ src/main/java/com/banc/securise/security/CustomUserDetails.java
@@ -0,0 +1,38 @@
+package com.banc.securise.security;
+
+import com.banc.securise.entity.User;
+import com.banc.securise.enums.Role;
+import com.banc.securise.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Service
+@AllArgsConstructor
+public class CustomUserDetails implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
+        User user = userRepository.findByEmail(username).orElseThrow(()-> new UsernameNotFoundException(username));
+
+        GrantedAuthority authority = new SimpleGrantedAuthority("ROLE_"+user.getRole());
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getEmail())
+                .password(user.getPassword())
+                .authorities(Collections.singletonList(authority))
+                .accountExpired(false)
+                .accountLocked(false)
+                .credentialsExpired(false)
+                .disabled(false)
+                .build();
+    }
+}
