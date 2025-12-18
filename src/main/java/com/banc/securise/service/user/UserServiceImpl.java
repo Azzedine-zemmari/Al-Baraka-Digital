73db79b3caee90c057f6c11ae0096de70b90e191
Azzedine-zemmari <azzedinezemmari@gmail.com>
2025-12-18 10:21:27 +0100

    Create the register service method

--- src/main/java/com/banc/securise/service/user/UserServiceImpl.java
+++ src/main/java/com/banc/securise/service/user/UserServiceImpl.java
@@ -6,6 +6,8 @@ import com.banc.securise.mapper.UserMapper;
 import com.banc.securise.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -16,10 +18,24 @@ import java.util.Optional;
 public class UserServiceImpl implements UserService{
     private UserRepository userRepository;
     private UserMapper userMapper;
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserServiceImpl(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Override
     public UserRegisterDto registerUser(UserRegisterDto userRegisterDto){
-
+            User user = userMapper.userRegisterToUser(userRegisterDto);
+            Optional<User> isUserExists =userRepository.findByEmail(user.getEmail());
+            if(isUserExists.isPresent()){
+                throw new RuntimeException("user already exists");
+            }
+            String hash = passwordEncoder.encode(userRegisterDto.getPassword());
+            user.setPassword(hash);
+            userRepository.save(user);
+            return userMapper.userToDto(user);
     }
 
 
