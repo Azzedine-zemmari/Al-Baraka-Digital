275b3166a2309202046255892c62a95f5fd8f0f6
Azzedine-zemmari <azzedinezemmari@gmail.com>
2025-12-20 13:42:04 +0100

    modifier operation mapper pour fixier un problem d insertion

--- src/main/java/com/banc/securise/mapper/OperationMapper.java
+++ src/main/java/com/banc/securise/mapper/OperationMapper.java
@@ -3,9 +3,12 @@ package com.banc.securise.mapper;
 import com.banc.securise.Dto.DepositeDto;
 import com.banc.securise.entity.Operation;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface OperationMapper {
+    @Mapping(source = "amount", target = "amount")
+    @Mapping(source = "type", target = "type")
     DepositeDto entityToDto(Operation operation);
     Operation dtoToEntity(DepositeDto depositeDto);
 }
