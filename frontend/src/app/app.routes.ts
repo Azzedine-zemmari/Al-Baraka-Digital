25a95bae973d40c7ba325320ea7b00d14baf8786
Azzedine-zemmari <azzedinezemmari@gmail.com>
2025-12-31 22:12:50 +0100

    add the register component with the front routing

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"register",component:RegisterComponent
+    },
+    {
+        path:'', redirectTo:"/register",pathMatch:'full'
+    }
+];
