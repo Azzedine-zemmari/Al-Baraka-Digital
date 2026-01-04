9541e0c81a6f5b22c02e5d1fc2ac0314420cc7d7
Azzedine-zemmari <azzedinezemmari@gmail.com>
2026-01-04 16:08:11 +0100

    add the client guaard and frontend security

--- frontend/services/auth.service.ts
+++ frontend/services/auth.service.ts
@@ -3,12 +3,30 @@ import {environment} from '../src/environments/environment';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AuthResponse } from '../src/interfaces/AuthResponse';
+import jwt_decode from "jwt-decode";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   constructor(private http: HttpClient) {}
+
+  getRole(): String | null{
+    const token = localStorage.getItem('auth_token') || sessionStorage.getItem('auth_token');
+    if(!token) return null;
+
+    const decode: any = jwt_decode(token);
+    return decode.role;
+  }
+
+  isClient(): boolean{
+    return this.getRole() === "ROLE_CLIENT";
+  }
+
+  isAuthenticated(): boolean {
+  const token = localStorage.getItem('auth_token') || sessionStorage.getItem('auth_token');
+  return !!token;
+}
 
   logout(){
     localStorage.removeItem('auth_token');
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { RegisterComponent } from './register.component';
 import { LoginComponent } from './login.component';
 import { UserPageComponent } from './userpage.component';
+import { DepositComponent } from './deposit.component';
+import { ClientGuard } from '../../guards/ClientGuard';
 
 export const routes: Routes = [
     {
@@ -12,6 +14,9 @@ export const routes: Routes = [
     },
     {
         path:"userpage" ,component:UserPageComponent
+    },
+    {
+        path:"deposit",component:DepositComponent,canActivate:[ClientGuard]
     },
     {
         path:'', redirectTo:"/register",pathMatch:'full'
