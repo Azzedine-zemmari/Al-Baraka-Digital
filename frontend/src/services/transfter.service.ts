ed97bcca8f6012b2761d3b0d9c602e41ddf09ab2
Azzedine-zemmari <azzedinezemmari@gmail.com>
2026-01-15 10:10:26 +0100

    ajouter transfer service

---
+++ frontend/src/services/transfter.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { environment } from "../environments/environment";
+import { TransferRequest } from "../interfaces/TransferRequest";
+
+@Injectable({ providedIn: "root" })
+
+export class Transfer{
+  constructor(private http: HttpClient) { }
+  
+  transfer(transferData : TransferRequest) {
+    const token =
+      localStorage.getItem("auth_token") ||
+      sessionStorage.getItem("auth_token");
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+    });
+    return this.http.post(
+      `${environment.apiUrl}/api/v1/transfer/`, transferData, { headers }
+    );
+    
+  }
+  
+}
